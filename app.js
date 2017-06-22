1dd67c7e9e141b6816cc78d5d2e1e1c209dfbce9
theallisonbrett <theallisonbrett@gmail.com>
2017-06-22 13:17:51 -0700

    Add AJAX boilerplate and test success

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
 function getYouTube() {
-  console.log("success");
+  $.ajax({
+    type: "GET",
+    url: "https://api.myjson.com/bins/wgonb",
+    dataType: "json",
+    success: function(response) {
+    console.log("success!");
+  },
+
+    error: function(xhr, status, e) { console.log(status, e); } });
+
+
 }
 
 
