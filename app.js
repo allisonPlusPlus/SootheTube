d8442d0d701bb0666e6943c929b4f18abe31604b
theallisonbrett <theallisonbrett@gmail.com>
2017-06-22 23:12:48 -0700

    Link first video

--- app.js
+++ app.js
@@ -11,24 +11,18 @@ function getYouTube(query) {
 }
 
 function displayResults(response) {
-    //
-    // var elem = $('.searchresult-template').children().clone();
-    //  var imageURL = response.items[0].snippet.thumbnails.medium.url;
-    //     elem.find('img').attr('src', imageURL);
-    //     $(".searchresult-template").append(elem);
-    //
-    //
+//Display thumbnails
 $("#output1").find("img").attr('src', response.items[0].snippet.thumbnails.medium.url);
-
 $("#output2").find("img").attr('src', response.items[1].snippet.thumbnails.medium.url);
-
 $("#output3").find("img").attr('src', response.items[2].snippet.thumbnails.medium.url);
-
 $("#output4").find("img").attr('src', response.items[3].snippet.thumbnails.medium.url);
-
 $("#output5").find("img").attr('src', response.items[4].snippet.thumbnails.medium.url);
-
 $("#output6").find("img").attr('src', response.items[5].snippet.thumbnails.medium.url);
+
+//Make thumbnails link to video
+var watchUrl = "https://www.youtube.com/watch?v=" + response.items[1].id.videoId;
+$("#output1").find('a').attr('href', watchUrl);
+console.log(response.items[1].id.videoId)
 
   console.log(response.items[0].snippet.thumbnails.medium.url);
 
