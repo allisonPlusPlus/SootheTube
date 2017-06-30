601389ca0a1d6c0b62d7cb6d001d1a1bae1727e3
theallisonbrett <theallisonbrett@gmail.com>
2017-06-30 09:55:55 -0700

    Beautify code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var url = "https://www.googleapis.com/youtube/v3/search";
 
 
 function getYouTube(query) {
-  var data = {
+    var data = {
         maxResults: '6',
         part: 'snippet',
         key: 'AIzaSyApCFcADbM3EgInOvuv2IevCLHYUDjaCfs',
@@ -15,37 +15,37 @@ function getYouTube(query) {
 
 function displayResults(response) {
 
-  function appendResults(item, i){
+    function appendResults(item, i) {
+        //Display thumbnail
+        $(`#output${i+1}`).find("img").attr('src', item.snippet.thumbnails.medium.url);
+
+        // //Make thumbnail link to video
+        $(`#output${i+1}`).find('a').attr('href', watchUrl + item.id.videoId);
+    }
+
+    $(".searchresult").removeClass("hidden");
     //Display thumbnail
-    $(`#output${i+1}`).find("img").attr('src', item.snippet.thumbnails.medium.url);
-
-    // //Make thumbnail link to video
-    $(`#output${i+1}`).find('a').attr('href', watchUrl + item.id.videoId);
-  }
-
-  $(".searchresult").removeClass("hidden");
-  //Display thumbnail
-  var watchUrl = "https://www.youtube.com/watch?v=";
+    var watchUrl = "https://www.youtube.com/watch?v=";
 
 
-// Mouse Hover Effect
-function hoverTitle(item, i) {
-  $(`#output${i+1}`).mouseover(function() {
-    $(`.title${i+1}` ).html(response.items[i].snippet.title );
-  });
-  $(`#output${i+1}`).mouseout(function() {
-    $("h2" ).empty();
-  });
-}
+    // Mouse Hover Effect
+    function hoverTitle(item, i) {
+        $(`#output${i+1}`).mouseover(function() {
+            $(`.title${i+1}`).html(response.items[i].snippet.title);
+        });
+        $(`#output${i+1}`).mouseout(function() {
+            $("h2").empty();
+        });
+    }
 
 
-  response.items.forEach(appendResults);
-  response.items.forEach(hoverTitle);
-  // for (let i=0; i < response.items.length; i++ ){
-  //   console.log(response.items[i])
-  //   $(`#output${i+1}`).find("img").attr('src', response.items[i].snippet.thumbnails.medium.url);
-  //   $(`#output${i+1}`).find('a').attr('href', watchUrl + response.items[i].id.videoId);
-  // }
+    response.items.forEach(appendResults);
+    response.items.forEach(hoverTitle);
+    // for (let i=0; i < response.items.length; i++ ){
+    //   console.log(response.items[i])
+    //   $(`#output${i+1}`).find("img").attr('src', response.items[i].snippet.thumbnails.medium.url);
+    //   $(`#output${i+1}`).find('a').attr('href', watchUrl + response.items[i].id.videoId);
+    // }
 
 
 }
@@ -57,14 +57,14 @@ function hoverTitle(item, i) {
 
 
 function formSubmit() {
-  $("form").submit(function(e) {
-    e.preventDefault();
-var query = $("#searchterm").val();
-getYouTube(query);
+    $("form").submit(function(e) {
+        e.preventDefault();
+        var query = $("#searchterm").val();
+        getYouTube(query);
 
-});
+    });
 }
 
 $(function() {
-  formSubmit();
+    formSubmit();
 });
