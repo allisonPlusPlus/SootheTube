83f5105c03532296e583a8e87310e5b9eb026b7c
theallisonbrett <theallisonbrett@gmail.com>
2017-06-29 20:31:18 -0700

    Remove hardcoding and replace with forEach loop

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+'use strict'
 var url = "https://www.googleapis.com/youtube/v3/search";
+
+
 
 function getYouTube(query) {
   var data = {
-        maxResults: '7',
+        maxResults: '6',
         part: 'snippet',
         key: 'AIzaSyApCFcADbM3EgInOvuv2IevCLHYUDjaCfs',
         q: query,
@@ -12,24 +15,27 @@ function getYouTube(query) {
 
 function displayResults(response) {
 
-$(".searchresult").removeClass("hidden");
+  function appendResults(item, i){
+    //Display thumbnail
+    $(`#output${i+1}`).find("img").attr('src', item.snippet.thumbnails.medium.url);
 
-//Display thumbnails
-$("#output1").find("img").attr('src', response.items[0].snippet.thumbnails.medium.url);
-$("#output2").find("img").attr('src', response.items[1].snippet.thumbnails.medium.url);
-$("#output3").find("img").attr('src', response.items[2].snippet.thumbnails.medium.url);
-$("#output4").find("img").attr('src', response.items[3].snippet.thumbnails.medium.url);
-$("#output5").find("img").attr('src', response.items[4].snippet.thumbnails.medium.url);
-$("#output6").find("img").attr('src', response.items[5].snippet.thumbnails.medium.url);
+    // //Make thumbnail link to video
+    $(`#output${i+1}`).find('a').attr('href', watchUrl + item.id.videoId);
+  }
 
-//Make thumbnails link to video
-var watchUrl = "https://www.youtube.com/watch?v=";
-$("#output1").find('a').attr('href', watchUrl + response.items[0].id.videoId);
-$("#output2").find('a').attr('href', watchUrl + response.items[1].id.videoId);
-$("#output3").find('a').attr('href', watchUrl + response.items[2].id.videoId);
-$("#output4").find('a').attr('href', watchUrl + response.items[3].id.videoId);
-$("#output5").find('a').attr('href', watchUrl + response.items[4].id.videoId);
-$("#output6").find('a').attr('href', watchUrl + response.items[5].id.videoId);
+  $(".searchresult").removeClass("hidden");
+  //Display thumbnail
+  var watchUrl = "https://www.youtube.com/watch?v=";
+
+
+  response.items.forEach(appendResults);
+
+  // for (let i=0; i < response.items.length; i++ ){
+  //   console.log(response.items[i])
+  //   $(`#output${i+1}`).find("img").attr('src', response.items[i].snippet.thumbnails.medium.url);
+  //   $(`#output${i+1}`).find('a').attr('href', watchUrl + response.items[i].id.videoId);
+  // }
+
 }
 
 
